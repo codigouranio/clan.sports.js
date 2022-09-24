56d486e376a94b4741f7f00b6a18ffbed1151a1d
Fabian Molina <molinafabian11b@gmail.com>
2022-09-23 23:07:36 -0500

    fix: add MateriaUi and Storybook Addon MaterialUI

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.(js|mdx)",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook-addon-material-ui"
   ],
   "framework": "@storybook/react",
   "core": {

be2636d1ba9de3709d4bb3694b7eaee5f1e37764
Wuelner Martínez <wuelner.martinez@outlook.com>
2022-11-29 15:46:07 -0600

    feat(frontend): storybook.js is configured for monorepo workspaces
    See issue #65

--- packages/frontend/.storybook/main.js
+++ packages/frontend/.storybook/main.js
@@ -1,20 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.(js|mdx)",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.(js|mdx)',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@chakra-ui/storybook-addon",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook-addon-material-ui"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "features": {
-    emotionAlias: false,
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+};

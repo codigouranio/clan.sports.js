20b86d231aeaf8d8b43a7ae4585342d7a20dd324
Wuelner Martínez <wuelner.martinez@outlook.com>
2022-11-29 18:02:29 -0600

    fix(frontend): fix a route in pages/index.tsx
    See issue #65

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,13 +1,13 @@
-import type { NextPage } from 'next'
-import { Heading } from '@chakra-ui/react'
+import type { NextPage } from 'next';
+import { Heading } from '@chakra-ui/react';
 
-import { MainLayout } from 'src/components/layouts/MainLayout'
+import { MainLayout } from '../components/layouts/MainLayout';
 
 const Home: NextPage = () => {
   return (
     <MainLayout>
       <Heading>Home Page</Heading>
     </MainLayout>
-  )
-}
-export default Home
+  );
+};
+export default Home;

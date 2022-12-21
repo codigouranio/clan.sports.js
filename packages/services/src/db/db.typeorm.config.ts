492ca9bae7c405ce83a0b75939b052af69ea61f6
Alejandro Alvarez <alejandro.alvarez.coder@gmail.com>
2022-12-21 00:16:22 -0300

    refactor: :card_file_box: move db files into the db folder
    Moved the migrations into the migrations folder
    Moved the db.typeorm.config.ts into the db folder
    Exported the typeorm config
    Changed the defauklt dababase extension to .sqlite instead of .sql
    The sqlite db now generates inside the db folder
    
    modified:   .gitignore
    new file:   packages/services/src/db/db.typeorm.config.ts
    new file:   packages/services/src/db/migrations/1664599246626-first.ts
    new file:   packages/services/src/db/migrations/1664600145225-first.ts
    new file:   packages/services/src/db/migrations/1665550166368-first.ts
    new file:   packages/services/src/db/migrations/1665720275563-first.ts
    
    Signed-off-by: Alejandro Alvarez <alejandro.alvarez.coder@gmail.com>

---
+++ packages/services/src/db/db.typeorm.config.ts
@@ -0,0 +1,29 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const dbConfig: DataSourceOptions = {
+  type: 'better-sqlite3',
+  name: 'connection',
+  database: `${__dirname}/db.sqlite`,
+  dropSchema: true,
+  synchronize: true,
+  migrations: [`${__dirname}/migrations/*.ts`],
+  entities: [`${__dirname}/../**/*.entity.js`],
+  migrationsTableName: 'migrations_typeorm',
+  migrationsRun: true,
+  cli: {
+    entitiesDir: `${__dirname}/entities`,
+    migrationsDir: `${__dirname}/migrations`,
+  },
+};
+
+export const testDbConfig: DataSourceOptions = {
+  ...dbConfig,
+  entities: [`${__dirname}/../**/*.entity.ts`],
+};
+
+export default new DataSource(dbConfig);
+
+export const dbConfigFactory = async (options) => {
+  const dataSource = await new DataSource(options).initialize();
+  return dataSource;
+};

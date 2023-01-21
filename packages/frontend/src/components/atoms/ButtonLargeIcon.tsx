2ba8ad25302d1bc75a1a38af93845fea9202ae57
Fabian Molina <molinafabian11b@gmail.com>
2023-01-21 04:25:35 -0500

    fix(frontend): added width prop to
    Signed-off-by: Fabian Molina <molinafabian11b@gmail.com>

--- packages/frontend/src/components/atoms/ButtonLargeIcon.tsx
+++ packages/frontend/src/components/atoms/ButtonLargeIcon.tsx
@@ -1,5 +1,5 @@
 /**
- * ButtonLargeIcon: button variant 'solid' that can have an icon on the left.
+ * ButtonLargeIcon: button variant 'solid' that can have an icon on the left large 100% width.
  * */
 import { Button } from '@chakra-ui/react';
 import { FiLogIn } from 'react-icons/fi';
@@ -8,12 +8,14 @@ interface Props {
   content: string;
   margin?: string | number;
   padding?: string | number;
+  width?: string | number;
 }
 
 export const ButtonLargeIcon: React.FC<Props> = ({
   content,
   margin,
   padding,
+  width,
 }) => {
   return (
     <Button
@@ -22,6 +24,7 @@ export const ButtonLargeIcon: React.FC<Props> = ({
       colorScheme="clan"
       m={margin || 'default'}
       p={padding || 'default'}
+      w={width || '100%'}
     >
       {content}
     </Button>
